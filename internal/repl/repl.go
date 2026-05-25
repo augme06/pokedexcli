d489020c350ab667e5ac69f7a8d261f005d3edd8
Augusto Gabriel <77197936+augme06@users.noreply.github.com>
2026-05-25 00:53:57 -0300

    refactor: refactor REPL to use ANSI sequences

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -6,17 +6,37 @@ import (
 	"log"
 	"os"
 
+	"github.com/augme06/pokedexcli/internal/ansi"
 	"github.com/augme06/pokedexcli/internal/pokeapi"
 )
+
+func printBanner() {
+	fmt.Println(ansi.Format(`
+▄██   ▄      ▄████████    ▄███████▄ 
+███   ██▄   ███    ███   ███    ███ 
+███▄▄▄███   ███    █▀    ███    ███ 
+▀▀▀▀▀▀███  ▄███▄▄▄       ███    ███ 
+▄██   ███ ▀▀███▀▀▀     ▀█████████▀  
+███   ███   ███    █▄    ███        
+███   ███   ███    ███   ███        
+ ▀█████▀    ██████████  ▄████▀                             
+	`, ansi.Red))
+	fmt.Println(ansi.Format("      Yet Another Pokedex\n", ansi.Faint))
+}
 
 // REPL logic
 func StartRepl() error {
 
 	var config pokeapi.Config
 
+	fmt.Print(ansi.AltScreen)
+	fmt.Print(ansi.Clear)
+
+	printBanner()
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Print("Pokedex > ")
+		fmt.Print(ansi.Format("Pokedex > ", ansi.Bold, ansi.Red))
 		scanner.Scan()
 		if scanner.Err() != nil {
 			log.Fatalf("%v", scanner.Err())
