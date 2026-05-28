8cdef01cf4fd9631f16634844e26f330b2690601
Augusto Gabriel <77197936+augme06@users.noreply.github.com>
2026-05-28 19:10:53 -0300

    feat: add party command

---
+++ internal/pokeapi/party.go
@@ -0,0 +1,44 @@
+package pokeapi
+
+import (
+	"fmt"
+
+	"github.com/augme06/pokedexcli/internal/ansi"
+)
+
+var party = make(map[string]Pokemon, 6)
+
+func GetParty() {
+	fmt.Println("Your party")
+	for _, p := range party {
+		fmt.Printf(ansi.Format(" - %s\n", ansi.Yellow), p.Name)
+	}
+}
+
+func AddToParty(name string) error {
+	if len(party) == 6 {
+		return fmt.Errorf("Party is full!")
+	}
+
+	p, ok := Pokedex[name]
+	if !ok {
+		return fmt.Errorf("You don't have have that Pokemon!")
+	}
+
+	if _, ok := party[name]; ok {
+		return fmt.Errorf("Pokemon already in the party!")
+	}
+
+	party[name] = p
+	return nil
+}
+
+func RemoveFromParty(name string) error {
+	_, ok := party[name]
+	if !ok {
+		return fmt.Errorf("Pokemon not found!")
+	}
+
+	delete(party, name)
+	return nil
+}
