ddfc34c41b67061f47fa46752c8e1924ea543bda
Augusto Gabriel <77197936+augme06@users.noreply.github.com>
2026-05-28 12:42:21 -0300

    refactor: refactor inspect output with ANSI sequences

--- internal/pokeapi/utils.go
+++ internal/pokeapi/utils.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+
+	"github.com/augme06/pokedexcli/internal/ansi"
 )
 
 func CleanInput(text string) []string {
@@ -21,15 +23,16 @@ func AddToPokedex(pokemon Pokemon) {
 }
 
 func FormatOutput(pokemon Pokemon) {
-	fmt.Printf("Name: %s\n", pokemon.Name)
-	fmt.Printf("Height: %d\n", pokemon.Height)
-	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Println("Name: " + ansi.Format(pokemon.Name, ansi.Yellow))
+	fmt.Println("Height: " + fmt.Sprintf(ansi.Format("%d", ansi.Yellow), pokemon.Height))
+	fmt.Println("Weight: " + fmt.Sprintf(ansi.Format("%d", ansi.Yellow), pokemon.Weight))
 	fmt.Println("Stats:")
 	for _, s := range pokemon.Stats {
-		fmt.Printf("  -%s: %d\n", s.StatInfo.Name, s.BaseStat)
+		fmt.Printf("  -%s: ", s.StatInfo.Name)
+		fmt.Printf(ansi.Format("%d\n", ansi.Blue, ansi.Bold), s.BaseStat)
 	}
 	fmt.Println("Types:")
 	for _, t := range pokemon.Type {
-		fmt.Printf("  - %s\n", t.Type.Name)
+		fmt.Printf(ansi.Format("  - %s\n", ansi.Bold), t.Type.Name)
 	}
 }
